1e0e1436a14c3b5cfb7bc13ea7cce81d12389501
sajagshrestha <sajagshrestha0852@gmail.com>
2023-08-01 15:26:35 +0545

    add keen-slider

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -1,13 +1,59 @@
-const Images=[
-  {
-    id: 1,
-    src:"/images/"
-  }
-]
-const ImageCarousel = () => {
-  return (
-    <div>Heelllloooo</div>
-  );
+import React from 'react';
+import {useKeenSlider} from 'keen-slider/react';
+import 'keen-slider/keen-slider.min.css';
+
+interface IContent {
+  text: (() => JSX.Element) | string;
+  image: string;
 }
+
+const Content: IContent[] = [
+  {
+    text: () => (
+      <p>
+        hello <span className="text-red">RED word</span> world
+      </p>
+    ),
+    image: '/images/bfi1.jpg'
+  },
+  {
+    text: 'hello world',
+    image: '/images/bfi2.jpg'
+  },
+  {
+    text: 'hello world',
+    image: '/images/bfi3.jpg'
+  },
+  {
+    text: 'hello world',
+    image: '/images/bfi4.jpg'
+  }
+];
+const ImageCarousel = () => {
+  const [sliderRef] = useKeenSlider(
+    {
+      loop: true
+    },
+    [
+      (slider) => {
+        const moveToNextSlide = () => slider.next();
+        setInterval(moveToNextSlide, 3000);
+      }
+    ]
+  );
+
+  return (
+    <div ref={sliderRef} className="keen-slider">
+      {Content.map((item, index) => (
+        <div className="w-screen h-[500px] bg-secondary keen-slider__slide" key={index}>
+          <img className="w-full h-full object-cover absolute" src={item.image} alt="" />
+          <div className="relative top-20 left-20 text-3xl font-bold text-white">
+            {typeof item.text === 'string' ? item.text : item.text()}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ImageCarousel;
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
-import { ROUTES } from "../constants";
-import Link from "next/link";
+import {useState} from 'react';
+import {ROUTES} from '../constants';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const MobileMenu: React.FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,29 +10,25 @@ const MobileMenu: React.FC = () => {
       <div className="flex w-11/12 items-center justify-end">
         <div
           className="group relative z-50 flex h-6 w-6 cursor-pointer flex-col items-center justify-between md:hidden "
-          onClick={() => setIsOpen(!isOpen)}
-        >
+          onClick={() => setIsOpen(!isOpen)}>
           <span
             className={`h-1 w-full transform cursor-pointer rounded-lg bg-black transition duration-300 ease-in-out group-hover:text-red ${
-              isOpen ? "translate-y-2.5 rotate-45" : ""
-            }`}
-          ></span>
+              isOpen ? 'translate-y-2.5 rotate-45' : ''
+            }`}></span>
           <span
             className={`h-1 w-full transform cursor-pointer rounded-lg bg-black transition duration-300 ease-in-out group-hover:text-red ${
-              isOpen ? "w-0 opacity-0" : "w-full"
-            }`}
-          ></span>
+              isOpen ? 'w-0 opacity-0' : 'w-full'
+            }`}></span>
           <span
             className={`group-hover:text-red-500 h-1 w-full transform cursor-pointer rounded-lg bg-black transition duration-300 ease-in-out ${
-              isOpen ? "-translate-y-2.5 -rotate-45" : ""
-            }`}
-          ></span>
+              isOpen ? '-translate-y-2.5 -rotate-45' : ''
+            }`}></span>
         </div>
       </div>
       {isOpen && (
         <div className="fixed left-0 top-0 h-full w-full bg-white">
           {Object.entries(ROUTES).map(([key, value]) => (
-            <Link href={value}>
+            <Link href={value} key={key}>
               <div onClick={() => setIsOpen(false)} className="text-red">
                 {key}
               </div>
@@ -44,32 +41,32 @@ const MobileMenu: React.FC = () => {
 };
 const DesktopMenu: React.FC = () => {
   return (
-    <>
-    <div className="w-full h-[50px] bg-black hidden md:flex justify-between">
-        <img src="/images/BFI-logo.ico" alt="logo" width="90" height="32" className="ml-10" />
+    <nav className="w-full h-[50px] bg-black hidden md:flex justify-between">
+      <div className="px-6 ml-6">
+        <Image src="/images/BFI-logo.ico" alt="logo" width={20} height={20} className='object-contain'/>
+      </div>
       <div className="flex flex-row gap-10 items-center justify-center mr-10">
-      <Link href={"/"}>
-        <div className="text-white">Home</div>
-      </Link>
-      <Link href={"/about"}>
-        <div className="text-white">About</div>
-      </Link>
-      <Link href={"/projects"}>
-        <div className="text-white">Projects</div>
-      </Link>
-      <Link href={"/services"}>
-        <div className="text-white">Services</div>
-      </Link>
-      <Link href={"/carrer"}>
-        <div className="text-white">Carrer</div>
-      </Link>
-      <Link href={"/contact"}>
-        <div className="text-white">Contact</div>
-      </Link>
-    </div>
-    </div>
-    </>
-  )
+        <Link href={'/'}>
+          <div className="text-white">Home</div>
+        </Link>
+        <Link href={'/about'}>
+          <div className="text-white">About</div>
+        </Link>
+        <Link href={'/projects'}>
+          <div className="text-white">Projects</div>
+        </Link>
+        <Link href={'/services'}>
+          <div className="text-white">Services</div>
+        </Link>
+        <Link href={'/carrer'}>
+          <div className="text-white">Carrer</div>
+        </Link>
+        <Link href={'/contact'}>
+          <div className="text-white">Contact</div>
+        </Link>
+      </div>
+    </nav>
+  );
 };
 
 const Navbar = () => {
