7581079adda42f2eee5ce6b519d39d3ad8375eb5
Dipesh chauhan <dpshchhn@gmail.com>
2024-04-07 17:53:00 +0545

    fix: uncommented

--- src/pages/carrer/index.tsx
+++ src/pages/carrer/index.tsx
@@ -1,17 +1,17 @@
-// import Layout from "@/components/layout/Layout";
-// import Container from "@/components/layout/Container";
+import Layout from "@/components/layout/Layout";
+import Container from "@/components/layout/Container";
 
-// const Carrer: React.FC = () => {
-//   return (
-//     <Layout>
-//         <Container>
-//           <div className="mt-24">
-//             <a href="https://docs.google.com/forms/d/e/1FAIpQLSf-0TUPo_WHDfaYlJLOvrMmo7yoUjBXIJDorZY3OFVZ3SYAiQ/viewform?usp=sf_link">
-//               Apply now
-//             </a>
-//           </div>
-//         </Container>
-//     </Layout>
-//   );
-// };
-// export default Carrer;
+const Carrer: React.FC = () => {
+  return (
+    <Layout>
+        <Container>
+          <div className="mt-24">
+            <a href="https://docs.google.com/forms/d/e/1FAIpQLSf-0TUPo_WHDfaYlJLOvrMmo7yoUjBXIJDorZY3OFVZ3SYAiQ/viewform?usp=sf_link">
+              Apply now
+            </a>
+          </div>
+        </Container>
+    </Layout>
+  );
+};
+export default Carrer;
